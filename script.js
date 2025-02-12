56a7d629878c387f27b8e5d8f4c08bd345f36d27
kairblarson <kairblarson@gmail.com>
2025-02-12 16:27:49 -0600

    worked on menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() { 
+    let isMenuActive = false;
+
     $(".menu-container").click(function() {
         console.log($("#menuCheckbox").prop("checked"));
         if($("#menuCheckbox").prop("checked") == true) {
@@ -7,4 +9,36 @@ $( document ).ready(function() {
             $("#menu").animate({ left: "+=100px" }, "slow");
         }
     })
+
+    const heightOfVideo = $("#content-video").height();
+    $(".content-gif-overlay").height(heightOfVideo);
+    $("#unfocused-div").height(heightOfVideo);
+    const widthOfMenu = $("#mobile-menu-container").width();
+    $("#mobile-menu-container").css({
+        "top": "0px",  
+        "right": -(widthOfMenu+100)  
+      });
+
+    $(window).resize(function() {
+        // console.log("Window resized to: " + $(window).width() + "x" + $(window).height());
+        $(".content-gif-overlay").height($(window).height());
+    });
+
+    $("#menu-container").click(function() {
+        const widthOfMenu = $("#mobile-menu-container").width();
+
+        if(isMenuActive) {
+            console.log("HERE 1")
+            $("#mobile-menu-container").animate({ right: -200 }, "slow");
+            // $("#mobile-menu-container").css("display", "none");
+            $("unfocused-div").css("display", "none");
+            isMenuActive = false;
+        } else {
+            console.log("HERE 2")
+            $("#mobile-menu-container").animate({ right: "0px" }, "slow");
+            $("#mobile-menu-container").css("display", "flex");
+            $("unfocused-div").css("display", "block");
+            isMenuActive = true;
+        }
+    });
 });
