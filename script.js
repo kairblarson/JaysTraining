b17d0ad4f2b0280b88e9b27d69b7efc43da7fb1a
kairblarson <kairblarson@gmail.com>
2025-02-18 16:02:14 -0600

    created merch area

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ $( document ).ready(function() {
         }
     });
 
+    //main nav buttons
     $("#content-programs-btn").click(function() {
         $('html, body').animate({
             scrollTop: $('.content-box-3').offset().top - 100
@@ -60,4 +61,80 @@ $( document ).ready(function() {
             scrollTop: $('.content-box-4').offset().top - 100
         }, 1000);
     })
+
+    $("#nav-button-home").click(function() {
+        $('html, body').animate({
+            scrollTop: $('#main').offset().top
+        }, 1000);
+    })
+
+    $("#nav-button-programs").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-3').offset().top - 100
+        }, 1000);
+    })
+
+    $("#nav-button-aboutme").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-2').offset().top - 100
+        }, 1000);
+    })
+
+    $("#nav-button-challenges").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-5').offset().top - 100
+        }, 1000);
+    })
+
+    $("#nav-button-contactme").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-4').offset().top
+        }, 1000);
+    })
+    //
+
+    //mobile nav buttons
+    $("#mobile-menu-home").click(function() {
+        $('html, body').animate({
+            scrollTop: $('#main').offset().top
+        }, 1000);
+        hideMobileMenu();
+    })
+
+    $("#mobile-menu-programs").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-3').offset().top
+        }, 1000);
+        hideMobileMenu();
+    })
+
+    $("#mobile-menu-aboutme").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-2').offset().top
+        }, 1000);
+        hideMobileMenu();
+    })
+
+    $("#mobile-menu-challenges").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-5').offset().top
+        }, 1000);
+        hideMobileMenu();
+    })
+
+    $("#mobile-menu-contactme").click(function() {
+        $('html, body').animate({
+            scrollTop: $('.content-box-4').offset().top
+        }, 1000);
+        hideMobileMenu();
+    })
+    //
+
+    function hideMobileMenu() {
+        $("#mobile-menu-container").animate({ right: -200 }, "slow");
+        // $("#mobile-menu-container").css("display", "none");
+        $("#unfocused-div").css("display", "none");
+        $(".menu-bar").css("background-color", "gainsboro");
+        isMenuActive = false;
+    }
 });
